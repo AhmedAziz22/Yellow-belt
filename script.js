cab0c64b516b0d00c3c6f2638c3cc1773727d5b7
Ahmedaziz22 <Ahmed91884@hotmail.com>
2022-01-31 12:19:12 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function changeimage (elemnt){
+    elemnt.src = "succulents-2.jpg";
+}
+function changeimageback(element){
+    element.src ="succulents-1.jpg"
+}
+function Cookie() {
+    cookieBox.remove();
+}
+
+var cookieBox = document.querySelector(".cookie-policy");
